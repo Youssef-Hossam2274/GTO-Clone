1df28e88f9b3d8b705da582a97af3c57637e2b77
Youssef-Hossam2274 <y.hossam2274@gmail.com>
2024-07-10 11:56:15 +0300

    fix bugs about styling in Assistant component

--- component/Assistant/index.tsx
+++ component/Assistant/index.tsx
@@ -1,29 +1,32 @@
 import React from 'react';
 import "./styles.css";
 import Image from 'next/image';
-// import assistantImage from "../images/personal-travel-img-one.svg"
 import assistantImage from "@/images/personal-travel-img-one.svg"
 
 export default function Assistant() {
     return (
         <div className='assistant-component'>
-            <div className="team-information">
-                <p className='team-information-title'>PERSONAL TRAVEL ASSISTANT TEAM</p>
-                <p className="team-information-description">
-                    Our dedicated Personal Travel Assistant Team is on hand 7 days a week to help our members
-                    with their enquiries and bookings. All members need to do is to send us a 
-                    detailed screenshot or link of the lowest price they can find online and we will take 
-                    care of the rest.
-                </p>
+            <div className="assistant-bigBox">
 
-                <div className="personal-information">
-                    For more information, please contact your Personal Travel Assistant Team via <br />
-                    Email on info@GTOmembers.com <br />
-                    Phone/SMS/WhatsApp on +44 (0) 7393912090
+                <div className="team-information">
+                    <p className='team-information-title'>PERSONAL TRAVEL ASSISTANT TEAM</p>
+                    <p className="team-information-description">
+                        Our dedicated Personal Travel Assistant Team is on hand 7 days a week to help our members
+                        with their enquiries and bookings. All members need to do is to send us a 
+                        detailed screenshot or link of the lowest price they can find online and we will take 
+                        care of the rest.
+                    </p>
+
+                    <div className="personal-information">
+                        For more information, please contact your Personal Travel Assistant Team via <br />
+                        Email on info@GTOmembers.com <br />
+                        Phone/SMS/WhatsApp on +44 (0) 7393912090
+                    </div>
                 </div>
-            </div>
-            <div className="team-image">
-                <Image src={assistantImage} alt='assistantImage'/>
+                <div className="team-image">
+                    <Image src={assistantImage} alt='assistantImage'/>
+                </div>
+
             </div>
         </div>
     )
