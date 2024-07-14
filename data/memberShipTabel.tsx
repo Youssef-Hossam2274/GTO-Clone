9d31c627b9d9f6a7fa1aeef749dfab708a5a9277
Youssef-Hossam2274 <y.hossam2274@gmail.com>
2024-07-14 14:06:17 +0300

    saving the data in member ship types page

---
+++ data/memberShipTabel.tsx
@@ -0,0 +1,191 @@
+import exp from "constants"
+
+const TABEL = [
+    {
+        id:1,
+        title:"Hotel Instant Discount On Refundable Bookings (1st Room)",
+        rows:[
+            {
+                rowTitle:"4 - 12 months",
+                col1:"10%",
+                col2:"20%",
+                col3:"30%",
+                col4:"40%",
+            },
+            {
+                rowTitle:"1 - 4 months",
+                col1:"7%",
+                col2:"15%",
+                col3:"25%",
+                col4:"35%",
+            },
+            {
+                rowTitle:"up to 1 month",
+                col1:"5%",
+                col2:"10%",
+                col3:"10%",
+                col4:"15%",
+            },
+            {
+                rowTitle:"Extra Room(s)",
+                col1:"5%",
+                col2:"5%",
+                col3:"5%",
+                col4:"5%",
+            },
+        ]
+    },
+    {
+        id:2,
+        title:"Hotel Instant Discount On Non-Refundable Bookings",
+        rows:[
+            {
+                rowTitle:"Non-Refundable Bookings",
+                col1:"5%",
+                col2:"5%",
+                col3:"5%",
+                col4:"5%",
+            },
+        ]
+    },
+    {
+        id:3,
+        title:"Hotel Credit When Paying At The Property",
+        rows:[
+            {
+                rowTitle:"Pay at the property",
+                col1:"5%",
+                col2:"5%",
+                col3:"5%",
+                col4:"5%",
+            },
+        ]
+    },
+    {
+        id:4,
+        title:"Flight Credit",
+        rows:[
+            {
+                rowTitle:"Member's Economy Class",
+                col1:"2%",
+                col2:"5%",
+                col3:"7%",
+                col4:"8%",
+            },
+            {
+                rowTitle:"Member's Business Class",
+                col1:"1%",
+                col2:"2.5%",
+                col3:"3%",
+                col4:"4%",
+            },
+        ]
+    },
+    {
+        id:5,
+        title:"Attractions, Activities & Experiences Instant Discount",
+        rows:[
+            {
+                rowTitle:"Member's Ticket",
+                col1:"5%",
+                col2:"10%",
+                col3:"15%",
+                col4:"20%",
+            },
+            {
+                rowTitle:"Extra Ticket(s)",
+                col1:"5%",
+                col2:"5%",
+                col3:"5%",
+                col4:"0%",
+            },
+        ]
+    },
+    {
+        id:6,
+        title:"Theme Parks and Musicals Credit",
+        rows:[
+            {
+                rowTitle:"Member's Ticket",
+                col1:"5%",
+                col2:"10%",
+                col3:"15%",
+                col4:"20%",
+            },
+        ]
+    },
+    {
+        id:7,
+        title:"Car Rental , Transfers and Trains Credit",
+        rows:[
+            {
+                rowTitle:"Member's",
+                col1:"5%",
+                col2:"10%",
+                col3:"15%",
+                col4:"20%",
+            },
+        ]
+    },
+    {
+        id:8,
+        title:"Ferries and Coaches Instant Discount",
+        rows:[
+            {
+                rowTitle:"Member's Ticket",
+                col1:"5%",
+                col2:"10%",
+                col3:"15%",
+                col4:"20%",
+            },
+            {
+                rowTitle:"Extra Ticket(s)",
+                col1:"5%",
+                col2:"5%",
+                col3:"5%",
+                col4:"5%",
+            },
+        ]
+    },
+    {
+        id:9,
+        title:"Cruises Credit",
+        rows:[
+            {
+                rowTitle:"Member's Cabin",
+                col1:"2.5%",
+                col2:"5%",
+                col3:"5%",
+                col4:"5%",
+            },
+        ]
+    },
+    {
+        id:10,
+        title:"Airport Lounges Access Credit",
+        rows:[
+            {
+                rowTitle:"Selected Lounges​",
+                col1:"2.5%",
+                col2:"5%",
+                col3:"5%",
+                col4:"5%",
+            },
+        ]
+    },
+    {
+        id:11,
+        title:"Shopping, Eating & Drinking (more than 30 countries)",
+        rows:[
+            {
+                rowTitle:"30 Countries+ (March 2023)",
+                col1:"Complimentary",
+                col2:"Complimentary",
+                col3:"Complimentary",
+                col4:"Complimentary",
+            },
+        ]
+    },
+]
+
+export default TABEL;
