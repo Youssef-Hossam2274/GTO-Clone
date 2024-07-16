4638477770f44f944d507d5f03ed0897bd8531c1
Youssef-Hossam2274 <y.hossam2274@gmail.com>
2024-07-16 16:09:11 +0300

    finish the FAQs page and make it resposive for all screens

--- app/FAQs/page.tsx
+++ app/FAQs/page.tsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import "./page.css"
+import FAQs from "@/data/faqs"
 
 export default function page() {
     return (
-        <div>
-            
+        <div className='faqs-component'>
+            <p className="faqs-title">
+                FAQs
+            </p>
+            <div className="all-questions">
+                {
+                    FAQs.map(faqsDetail =>{
+                        return(
+                            <div className="faqs-detail" key={faqsDetail.id}>
+                                <div className="quest-answer">
+                                    <p>{faqsDetail.quest}</p>
+                                    <p>{faqsDetail.ans}</p>
+                                </div>
+                                <div className="faqs-steps">
+                                    {
+                                        faqsDetail.steps.map(stepDetail =>{
+                                            return (
+                                                <p key={faqsDetail.id} className='step-detail'>{stepDetail}</p>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
--- data/faqs.tsx
+++ data/faqs.tsx
@@ -1,3 +1,5 @@
+import exp from "constants";
+
 const FAQS = [
     {
         id: 1,
@@ -8,6 +10,157 @@ const FAQS = [
             "Decide what type of membership is best for you",
             "Choose the card that best suits your travel needs and budget",
             "Complete the online form including the invitation code if you have one",
+            "Make payment online",
+            "You will receive your digital membership package via email. You can also download our app by searching for GTO Members on the App store.",
+            "Welcome to GTO membership, you can now start to enjoy your benefits"
         ]
-    }
-]
+    },
+    {
+        id: 2,
+        quest: "How do I decide which membership is most suitable for me?",
+        ans: "There are 3 types of membership, Discount, Luxury Concierge and Discount and Luxury Concierge Card. Please follow below 3 steps to choose best suited card:",
+        steps:[
+            "Go to \"Types of Membership\" to see the different types of membership available",
+            "Decide what type of membership is best for you",
+            "Choose the card that best suits your travel needs and budget"
+        ]
+    },
+    {
+        id: 3,
+        quest: "How do I pay for my membership fee?",
+        ans: "Go to the \"Become a Member\" section, choose the type of membership best suited for you, complete the registration and make payment. We accept payment via credit card, American Express and PayPal.",
+        steps:[
+            ""
+        ]
+    },
+    {
+        id: 4,
+        quest: "Is there a physical card?",
+        ans: "No, it is a digital card only.",
+        steps:[
+            ""
+        ]
+    },
+    {
+        id: 5,
+        quest: "Is there a mobile app for the card?",
+        ans: "Yes, please search for GTO Members in the app store.",
+        steps:[
+            ""
+        ]
+    },
+    {
+        id: 6,
+        quest: "I haven't received my digital membership package, what shall I do?",
+        ans: "Please contact Personal Travel Assistant Team via email on info@GTOmembers.com or via Phone/SMS/WhatsApp on +44 (0) 7496 522 253 or +44 (0) 7939 999 787",
+        steps:[
+            ""
+        ]
+    },
+    {
+        id: 7,
+        quest: "How do I make a booking?",
+        ans: "There are 3 simple steps: 1. Search major booking websites for the best price 2. Share the booking details with your Personal Travel Assistant Team to receive your discounted quotation 3. Make payment using the link on your quotation and receive your booking confirmation",
+        steps:[
+            ""
+        ]
+    },
+    {
+        id: 8,
+        quest: "What can I get discounts for?",
+        ans: "All Discount card members receive discount on Hotels, Flight Tickets, Attractions, Activities & Experience, Historical Landmarks and Museums, Entertainment and Theme Parks. Dependent on your level of membership you may also receive discount on Car Rental, Cinemas, Shopping, Restaurants and Airport Lounges.",
+        steps:[
+            ""
+        ]
+    },
+    {
+        id: 9,
+        quest: "How do I make payment for my bookings?",
+        ans: "When the quotation is sent to you there is a link that takes you to our secure payment page. We accept payment via credit card, American Express and PayPal.",
+        steps:[
+            ""
+        ]
+    },
+    {
+        id: 10,
+        quest: "When will I receive my booking confirmation?",
+        ans: "We will make the booking once the payment has been made and confirmation will be sent to you promptly. Please check all the booking details to ensure they are correct.",
+        steps:[
+            ""
+        ]
+    },
+    {
+        id: 11,
+        quest: "I didn't receive my booking confirmation, what shall I do?",
+        ans: "Please contact Personal Travel Assistant Team immediately via email on info@GTOmembers.com or via Phone/SMS/WhatsApp on +44 (0) 7496 522 253 or +44 (0) 7939 999 787.",
+        steps:[
+            ""
+        ]
+    },
+    {
+        id: 12,
+        quest: "If the details are wrong on my booking confirmation, what shall I do?",
+        ans: "Please contact your personal travel assistant team immediately to give them the correct details via email on info@GTOmembers.com or via Phone/SMS/WhatsApp on +44 (0) 7496 522 253 or +44 (0) 7939 999 787.",
+        steps:[
+            ""
+        ]
+    },
+    {
+        id: 13,
+        quest: "Am I able to book for someone else?",
+        ans: "You can book for immediate family and receive 50% of your normal discount.",
+        steps:[
+            "Immediate family is defined as the cardholder's parents, spouse and children up to 21 years old."
+        ]
+    },
+    {
+        id: 14,
+        quest: "",
+        ans: "",
+        steps:[
+            ""
+        ]
+    },
+    {
+        id: 15,
+        quest: "How do I upgrade to receive more benefits and discounts?",
+        ans: "Please contact Personal Travel Assistant Team via email on info@GTOmembers.com or via Phone/SMS/WhatsApp on +44 (0) 7496 522 253 or +44 (0) 7939 999 787",
+        steps:[
+            ""
+        ]
+    },
+    {
+        id: 16,
+        quest: "I have changed my mind and want to cancel my membership, can I get a refund?",
+        ans: "You can cancel your membership free of charge within 14 days of joining, provided that the membership benefits or discounts have not been used. After this period the membership is non-refundable.",
+        steps:[
+            ""
+        ]
+    },
+    {
+        id: 17,
+        quest: "If I don't use my card can I get a refund?",
+        ans: "No, the card is non-refundable.",
+        steps:[
+            ""
+        ]
+    },
+    {
+        id: 18,
+        quest: "Which languages can I use to communicate with the personal travel assistant team?",
+        ans: "You can communicate in English and Mandarin with the team in London or you can communicate in Arabic, French or English with the team in Cairo.",
+        steps:[
+            ""
+        ]
+    },
+    {
+        id: 19,
+        quest: "What if I have any further questions?",
+        ans: "Please contact info@GTOmembers.com or +44 7496 522253 with any questions that you may have.",
+        steps:[
+            ""
+        ]
+    },
+];
+
+export default FAQS;
