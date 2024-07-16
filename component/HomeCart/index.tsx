91d37dcea526a4df75a0cecc69e87e9fd4fea115
Youssef-Hossam2274 <y.hossam2274@gmail.com>
2024-07-16 19:32:47 +0300

    fix bugs in coverage and add some photos in home carts

--- component/HomeCart/index.tsx
+++ component/HomeCart/index.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 import Image, { StaticImageData } from "next/image";
 import "./styles.css";
-import cartImg1 from "@/images/home-cart-1.jpg";
+
+import cartImg1 from "@/images/home-cart-1.svg";
+import cartImg2 from "@/images/home-cart-2.svg";
+import cartImg3 from "@/images/home-cart-3.jpg";
+import cartImg4 from "@/images/home-cart-4.svg";
+import cartImg5 from "@/images/home-cart-5.jpg";
+import cartImg6 from "@/images/home-cart-6.jpg";
+import cartImg7 from "@/images/home-cart-7.svg";
+import cartImg8 from "@/images/home-cart-8.jpg";
 
 interface CartProps {
     id: number;
@@ -18,37 +26,37 @@ const CARTS = [
     },
     {
     id: 2,
-    image: cartImg1,
+    image: cartImg2,
     title: "Luxurious Products & Services",
     },
     {
     id: 3,
-    image: cartImg1,
+    image: cartImg3,
     title: "How To Receive Your Discounts",
     },
     {
     id: 4,
-    image: cartImg1,
+    image: cartImg4,
     title: "Personal Travel Assistant Team",
     },
     {
     id: 5,
-    image: cartImg1,
+    image: cartImg5,
     title: "How To Become a Member",
     },
     {
     id: 6,
-    image: cartImg1,
+    image: cartImg6,
     title: "Discounts & Benefits",
     },
     {
     id: 7,
-    image: cartImg1,
+    image: cartImg7,
     title: "Travel & Lifestyle Products & Services",
     },
     {
     id: 8,
-    image: cartImg1,
+    image: cartImg8,
     title: "Booking Online With Tripiology.com",
     },
 ];
