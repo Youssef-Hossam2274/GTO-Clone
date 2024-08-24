fa5ab1dc06698bcfeefcd7ff55ed35ea6bc6eff0
Youssef-Hossam2274 <y.hossam2274@gmail.com>
2024-08-24 19:57:42 +0300

    fix links in mobile view

--- component/NavBar/index.tsx
+++ component/NavBar/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import gto_logo from "@/images/gto-logo.png";
@@ -11,30 +11,20 @@ import navBarIcon from "@/images/navbar-menu.svg";
 
 
 export default function NavBar() {
-    let mobileLink = false;
-    const toggle = ()=>{
-        mobileLink = !mobileLink;
-        const navBar = document.querySelector(".nav-bar-component");
-        const mobile_links =document.querySelector(".mobile-links");
-        if(mobileLink === true){
-            navBar?.classList.add("mobile");
-            mobile_links?.classList.add("active");
-        }
-        else{
-            mobile_links?.classList.remove("active");
-            navBar?.classList.remove("mobile");
-        }
+    const [mobileToggle, setMobileToggle] = useState(false);
+    const toggle = () => {
+        setMobileToggle(!mobileToggle);
     }
     return (
-        <div className='nav-bar-component'>
+        <div className={`nav-bar-component ${mobileToggle ? "mobile" : ""}`}>
             <div className="nav-fullScreen">
                 <div className="left-box">
                     <div className="nav-logo">
-                        <Link href="/"> 
-                            <Image src={gto_logo} alt="gto-logo" /> 
+                        <Link href="/">
+                            <Image src={gto_logo} alt="gto-logo" />
                         </Link>
                     </div>
-                
+
                     <div className='urls'>
                         <Link href="Discounted_Products">Discounted Products</Link>
                         <Link href="Luxury_Products">Luxury Products</Link>
@@ -43,17 +33,17 @@ export default function NavBar() {
                         <Link href="How_to_join">How to join</Link>
                     </div>
                 </div>
-                
+
                 <div className="mobile-holder">
-                    <Image src={navBarIcon} className="bar-icon" alt='navBaricon' onClick={toggle}/>
+                    <Image src={navBarIcon} className="bar-icon" alt='navBaricon' onClick={toggle} />
                 </div>
-                
+
                 <div className="right-box">
-                    <Image className='EMER-logo' src={EMER} alt="EMER" style={{width:"150px"}} />
-                    <Image className='etisalate-logo'  src={etisalat} alt="etisalat" style={{width:"100px", height:"auto"}} />
+                    <Image className='EMER-logo' src={EMER} alt="EMER" style={{ width: "150px" }} />
+                    <Image className='etisalate-logo' src={etisalat} alt="etisalat" style={{ width: "100px", height: "auto" }} />
                 </div>
             </div>
-            <div className="mobile-links">
+            <div className={`mobile-links ${mobileToggle ? "active" : ""}`}>
                 <Link href="Discounted_Products" onClick={toggle}>Discounted Products</Link>
                 <Link href="Luxury_Products" onClick={toggle}>Luxury Products</Link>
                 <Link href="Membership_Types" onClick={toggle}>Membership Types</Link>
